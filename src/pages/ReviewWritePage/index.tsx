707627abaaa92f1875f479fdb84d20ad692566c7
freecoderer <110393847+freecoderer@users.noreply.github.com>
2023-10-02 01:38:43 +0900

    chore: axios installed

---
+++ src/pages/ReviewWritePage/index.tsx
@@ -0,0 +1,5 @@
+const ReviewWritePage = () => {
+  return <p>ReviewWritePage</p>;
+};
+
+export default ReviewWritePage;
---
+++ src/pages/SearchResult/index.tsx
@@ -0,0 +1,5 @@
+const SearchResult = () => {
+  return <p>SearchResult page</p>;
+};
+
+export default SearchResult;
