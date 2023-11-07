1bf095d00995aa0808210dd1ad59ca8a6077c20d
freecoderer <donggood7@naver.com>
2023-11-07 23:15:19 +0900

    feat: login completed with minimum functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout/Layout'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import './App.scss'
-import Signup from "./pages/Signup";
-
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from './components/Store/authSlice'; // authSlice에서 login 액션 생성자를 import 합니다.
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import './App.scss';
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+        const username = localStorage.getItem('username');
+        if (accessToken && username) {
+            dispatch(login({ accessToken, username })); // login 액션 생성자를 사용합니다.
+        }
+    }, [dispatch]);
+
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
@@ -14,7 +26,6 @@ function App() {
                 <Route index element={<Home />} />
                 <Route path="login" element={<Login />} />
                 <Route path="signup" element={<Signup />} />
-
             </Route>
         </Routes>
     )
