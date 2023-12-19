fca9b6f5749b6659f7f0a3cb8acad3233d8aecd5
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-12-19 23:40:36 +0900

    Design: 스타일 수정

--- src/pages/Search/search.tsx
+++ src/pages/Search/search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Filter from "../../components/Filter/filter";
 import Footer from "../../components/Footer/footer";
 import Nav from "../../components/Nav/nav";
